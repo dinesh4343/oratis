b43ee38e5d00744cea7e2be0e43d5e9f41904994
dinesh4343 <dineshpandian008@gmail.com>
2025-08-11 11:53:03 +0530

    push

--- src/components/JS/Hero.js
+++ src/components/JS/Hero.js
@@ -213,7 +213,7 @@ const VideoPopup = ({ videoUrl, onClose }) => {
         <button className="close-btn" onClick={onClose}>&times;</button>
         <video controls autoPlay muted playsInline width="100%">
           <source src={`${process.env.PUBLIC_URL}//TextToAudio.webm`} type="video/webm" />
-  <source src={`${process.env.PUBLIC_URL}//TextToAudio.mp4`} type="video/mp4" />
+          <source src={`${process.env.PUBLIC_URL}//TextToAudio.mp4`} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
